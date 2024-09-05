0cde5ef084a0a705304cdc967693e43a5f724e79
Elizaveta Veselova <veselovae1605@yandex.ru>
2024-09-05 17:14:23 +0300

    base replaced by publicPath in vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-    base: "/Platano-Fest",
+    publicPath: "/Platano-Fest",
     transpileDependencies: true,
 });
